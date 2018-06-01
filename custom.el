ccaf073ca0b2172bed3e6afed6855c2406e994cf
Haidong <zhd173@outlook.com>
2018-06-01 13:50:09 +0800

    add custom.el and some variables about company

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ ;; 自动补全相关变量
+ '(company-dabbrev-minimum-length 1)
+ '(company-dabbrev-other-buffers nil)
+ '(company-show-numbers t)
+ '(company-statistics-auto-restore nil)
+ '(company-tooltip-align-annotations t)
+
+ '(package-selected-packages
+   '(yasnippet-snippets helm-company helm-c-yasnippet fuzzy company-statistics company-restclient know-your-http-well company-emoji company-ansible company-anaconda company auto-yasnippet yasnippet ac-ispell auto-complete yapfify yaml-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org symon string-inflection spaceline-all-the-icons smeargle restclient-helm restart-emacs ranger rainbow-mode rainbow-identifiers rainbow-delimiters pyvenv pytest pyenv-mode py-isort prodigy popwin pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file ob-restclient ob-http neotree nameless mwim move-text mmm-mode markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum live-py-mode link-hint jinja2-mode indent-guide importmagic ibuffer-projectile hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-ag google-translate golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md font-lock+ flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu engine-mode emojify emoji-cheat-sheet-plus elisp-slime-nav editorconfig dumb-jump dockerfile-mode docker diminish define-word cython-mode counsel-projectile column-enforce-mode color-identifiers-mode clean-aindent-mode centered-cursor-mode auto-highlight-symbol auto-dictionary auto-compile ansible-doc ansible anaconda-mode aggressive-indent ace-window ace-link ace-jump-helm-line)))
+(custom-set-faces
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
