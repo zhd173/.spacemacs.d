0a471d437d630b8943818eadc1161a46dccc4092
张海东 <haidong@botpy.com>
2018-09-17 10:01:15 +0800

    update theme to spacemacs-dark

--- init.el
+++ init.el
@@ -198,8 +198,7 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         ;; spacemacs-dark
-                         doom
+                         spacemacs-dark
                          ;; molokai
                          ;; badwolf
                          ;; spacemacs-light
